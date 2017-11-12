0fb57302798b0ed3c784182227a92f3863b3ae55
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2017-11-12 11:36:39 +0200

    JXCoreService implemented

---
+++ src/main/java/io/jxcore/node/JXCoreService.java
@@ -0,0 +1,35 @@
+package io.jxcore.node;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.IOException;
+
+public final class JXCoreService extends Service {
+
+    private final String TAG = JXCoreService.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            final JXCore jxCoreClient = new JXCore(getApplicationContext());
+            jxCoreClient.start();
+        } catch (final IOException e) {
+            Log.e(TAG, "JXCore service not started", e);
+        }
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, final int flags, final int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(final Intent intent) {
+        return null;
+    }
+
+}
